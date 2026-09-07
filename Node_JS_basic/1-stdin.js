edd77946e67d6570875b8d9a38a4869de77db44a
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-09-06 21:55:26 -0400

    1-stdin.js: pass raw stdin data through unmodified (fix checker line-ending mismatch)

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,17 +1,9 @@
-const readline = require('readline');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-rl.on('line', (line) => {
-  process.stdout.write(`Your name is: ${line}\n`);
-});
-
-rl.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
