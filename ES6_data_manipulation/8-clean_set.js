2566ab1f37fd40dfd26a412ae20a1a17f0d2a29f
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-09-06 21:03:43 -0400

    ES6_data_manipulation: add task 8 (clean set)

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  const result = [];
+
+  set.forEach((value) => {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
+}
