69eabc785d53c60637ede1a0371d80ca6af0993d
Luis Felipe Gonzalez <lgonzalez05@gmail.com>
2026-09-06 21:51:03 -0400

    1-stdin.js: use readline to buffer input by line (fix chunked-input bug)

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,17 @@
+const readline = require('readline');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
 });
 
-process.stdin.on('end', () => {
+rl.on('line', (line) => {
+  process.stdout.write(`Your name is: ${line}\n`);
+});
+
+rl.on('close', () => {
   process.stdout.write('This important software is now closing\n');
 });
